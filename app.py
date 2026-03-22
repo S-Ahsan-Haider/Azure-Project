13a6442a31daa3f039a1363f44a81f959dbbe6f7
Syed Ahsan Haider <ahsanhaider124@gmail.com>
2026-03-22 23:10:35 +0530

    Added subtraction feature

--- app.py
+++ app.py
@@ -2,26 +2,34 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# Home page of my calculator
-
 @app.route('/')
 def home():
     return '''
         <h1>Cloud Calculator</h1>
-        <form action="/add" method="post">
-            <input type="number" name="num1" placeholder="First Number" required> +
+        <form method="post">
+            <input type="number" name="num1" placeholder="First Number" required>
             <input type="number" name="num2" placeholder="Second Number" required>
-            <button type="submit">Calculate</button>
+            <br><br>
+            <button type="submit" formaction="/add">Add (+)</button>
+            <button type="submit" formaction="/subtract">Subtract (-)</button>
         </form>
     '''
 
-# Calclation part
+# Addition
 
 @app.route('/add', methods=['POST'])
 def add():
     n1 = int(request.form['num1'])
     n2 = int(request.form['num2'])
-    return f"<h2>The Result is: {n1 + n2}</h2> <br> <a href='/'>Go Back</a>"
+    return f"<h2>Addition Result: {n1 + n2}</h2> <br> <a href='/'>Go Back</a>"
+
+# Subtraction
+
+@app.route('/subtract', methods=['POST'])
+def subtract():
+    n1 = int(request.form['num1'])
+    n2 = int(request.form['num2'])
+    return f"<h2>Subtraction Result: {n1 - n2}</h2> <br> <a href='/'>Go Back</a>"
 
 if __name__ == '__main__':
     app.run()
